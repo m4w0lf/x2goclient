6c7a731e7cd4ce1ec41e5ebe22832ac624c7f141
Mihai Moldovan <ionic@ionic.de>
2019-06-07 08:41:17 +0200

    Windows: add support for x3270-fonts, including adding a pretty nasty but necessary fonts.* file regeneration hook.

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -13,6 +13,15 @@ xcopy /E /Y    %1\MSVC-DLLs\2013-12.0.21005.1-x86\msvcr120.dll %3\ || exit /b  %
 xcopy /E /Y    %1\pulse\7.1-2.2_bin %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\PuTTY\0.71_bin %3\ || exit /b  %errorlevel%
 xcopy /E /Y /I %1\VcXsrv\1.20.1.4_bin %3\VcXsrv || exit /b  %errorlevel%
+xcopy /E /Y /I %1\x3270-fonts\3.6ga4\misc %3\VcXsrv\fonts\misc || exit /b  %errorlevel%
+xcopy /E /Y    %1\x3270-fonts\3.6ga4\copyright-x3270-fonts %3\VcXsrv\ || exit /b  %errorlevel%
+rem We have to call mkfontscale and mkfontdir after updating the misc font directory.
+rem Sadly this can't be done statically, so we need a usable Cygwin installation with
+rem the mentioned packages installed.
+pushd %3\VcXsrv\fonts\misc
+C:\cygwin.latest\bin\bash.exe -c "/usr/bin/mkfontdir"
+C:\cygwin.latest\bin\bash.exe -c "/usr/bin/mkfontscale"
+popd
 xcopy /E /Y    %1\zlib\1.2.8_bin\zlib1.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %2\bin\ssleay32.dll %3\ || exit /b  %errorlevel%
